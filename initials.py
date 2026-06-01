3f107f2c85e5e8de2709277f312d9087622c1f1b
Hakeem Baki <242541400@live.gctu.edu.gh>
2026-05-31 18:21:49 -0700

    Assignment 2

--- initials.py
+++ initials.py
@@ -1,7 +1,31 @@
-
 def main():
-  pass #code goes here
 
+# HB.py
+#Printing my initials DHB in Large block letters
+#Assignment 1
+#Hakeem Baki
+#2425401400
+#2425401400@live.gctu.edu.gh
+#Augustus
+#
+#On my honor, Hakeem Baki, this programing is my own work
+#and i have not provided this code to any other student.
 
+    print()
 
-main()
+print("...DHB")
+
+print()
+
+print("...DDDDDDDDDD..........HHH......HHH........BBBBBBBBBB.......")
+print("...DDD......DD.........HHH......HHH........BBB......BB......")
+print("...DDD.......DD........HHH......HHH........BBB.......BB.....")
+print("...DDD.......DD........HHH......HHH........BBB.......BB.....")
+print("...DDD.......DD........HHH......HHH........BBB.......BB.....")
+print("...DDD.......DD........HHHHHHHHHHHH........BBBBBBBBBBB......")
+print("...DDD.......DD........HHHHHHHHHHHH........BBBBBBBBBB.......")
+print("...DDD.......DD........HHH......HHH........BBB.......BB.....")
+print("...DDD......DD....**...HHH......HHH...**...BBB......BB....**")
+print("...DDDDDDDDDD.....**...HHH......HHH...**...BBBBBBBBBB.....**")
+
+print()
